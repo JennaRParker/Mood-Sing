a737547e0577ae969801b135a2b1c8ee7666cb80
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-08-21 20:47:56 -0400

    music links

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// The links below redirect me (with my API key for last.fm) to similar songs as a song of my own selection. After researching the best songs to qualify distinctive music generations, through process of elimination, I am the links to genre variables 
+
+// songs like "Dancing Queen" by Abba
+const happyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=abba&track=dancing+queen&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+
+// songs like "Let Her Go" by Passenger
+const sadMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=passenger&track=let+her+go&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+
+// songs like "Banana Pancakes" by Jack Johnson
+const peacefulMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=jack+johnson&track=banana+pancakes&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+
+// songs like "Unchained Melody" by The Rhighteous Brothers
+const romanticMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+righteous+brothers&track=unchained+melody&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+
+// songs like "Slow Motion" by Trey Songz
+const sexyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=trey+songz&track=slow+motion&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+
+//songs like "Bad Kids" by The Black Lips
+const angryMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+black+lips&track=bad+kids&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json' 
+
+// songs like Halloween Theme by John Carpenter
+const scaryMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=john+carpenter&track=halloween+theme&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
