b446c184d1b1a4008312d372e9c826603484ecac
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-08-22 16:40:42 -0400

    party rock anthem as the happy song playlist radio

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // The links below redirect me (with my API key for last.fm) to similar songs as a song of my own selection. After researching the best songs to qualify distinctive music generations, through process of elimination, I am the links to genre variables 
 
-// songs like "Dancing Queen" by Abba
-const happyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=abba&track=dancing+queen&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+// songs like "Party Rock Anthem" by LMFAO
+const happyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=lmfao&track=party+rock+anthem&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // songs like "Let Her Go" by Passenger
 const sadMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=passenger&track=let+her+go&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
@@ -18,7 +18,7 @@ const sexyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&art
 //songs like "Bad Kids" by The Black Lips
 const angryMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+black+lips&track=bad+kids&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
-// songs like Halloween Theme by John Carpenter
+// songs like "Halloween Theme" by John Carpenter
 const scaryMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=john+carpenter&track=halloween+theme&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // The links below direct me (with my API key to Big Huge Thesaurus) to an array of synonyms for specified words. 
