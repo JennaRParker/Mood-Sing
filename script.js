9f9236045357bb8993e445e16513e50fcc907982
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-08-25 13:08:56 -0400

    https vs http

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // The links below redirect me (with my API key for last.fm) to similar songs as a song of my own selection. After researching the best songs to qualify distinctive music generations, through process of elimination, I am the links to genre variables 
 
 // songs like "Party Rock Anthem" by LMFAO
-const happyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=lmfao&track=party+rock+anthem&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const happyMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=lmfao&track=party+rock+anthem&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // songs like "Let Her Go" by Passenger
-const sadMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=passenger&track=let+her+go&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const sadMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=passenger&track=let+her+go&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // songs like "Banana Pancakes" by Jack Johnson
-const peacefulMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=jack+johnson&track=banana+pancakes&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const peacefulMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=jack+johnson&track=banana+pancakes&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // songs like "Unchained Melody" by The Rhighteous Brothers
-const romanticMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+righteous+brothers&track=unchained+melody&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const romanticMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+righteous+brothers&track=unchained+melody&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // songs like "Slow Motion" by Trey Songz
-const sexyMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=trey+songz&track=slow+motion&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const sexyMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=trey+songz&track=slow+motion&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 //songs like "Bad Kids" by The Black Lips
-const angryMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+black+lips&track=bad+kids&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const angryMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=the+black+lips&track=bad+kids&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // songs like "Halloween Theme" by John Carpenter
-const scaryMusic = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=john+carpenter&track=halloween+theme&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
+const scaryMusic = 'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=john+carpenter&track=halloween+theme&api_key=1bc71772e81d1ed9594a4ad8921042ba&format=json'
 
 // The links below direct me (with my API key to Big Huge Thesaurus) to an array of synonyms for specified words. 
 
